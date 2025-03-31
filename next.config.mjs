724ca55cd182e58269a48ab26e5f153bc0187667
pawantech12 <pawankumavat042@gmail.com>
2025-03-31 16:35:30 +0530

    done with overall Saas website development and ready for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["cdn.dribbble.com"], // Add the external image domain here
+  },
+};
 
 export default nextConfig;
